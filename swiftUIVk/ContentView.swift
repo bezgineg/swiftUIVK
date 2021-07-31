f5f64c3722fa13cf4d83d5bbc8f2df4ac38e1cd1
BezginEvgenii <bezgineg@yandex.ru>
2021-07-31 12:00:20 +0300

    first commit

---
+++ swiftUIVk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIVk
+//
+//  Created by Евгений on 31.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIVk/swiftUIVkApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIVkApp.swift
+//  swiftUIVk
+//
+//  Created by Евгений on 31.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIVkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
